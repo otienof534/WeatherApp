974f756808c21ed5439d7824bb9738a7b06d9aa4
otienof534 <otienof534@gmail.com>
2023-01-21 20:14:16 +0300

    update having search box and exit button

--- WheatherAPI.py
+++ WheatherAPI.py
@@ -4,19 +4,32 @@ import json
 
 x = Tk()
 x.title('Weather Focus')
+x.iconbitmap('C:\\Users\\USER\\Downloads\\clouds.ico')
 x.geometry('700x600')
-location = input('enter location: ')
-data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid'
-                    f'=561873282dd9f39ea5aab7bd9f29e045')
-weather_data = json.loads(data.text)
-
 
 def exit_task():
     quit()
+exit_button = Button(x, text='Exit', pady=10, padx=20, fg='red', command=exit_task)
+exit_button.grid(row=1,column=0)
 
-Label(x, text=weather_data['weather']).pack()
+search = Entry(x,width=50,borderwidth=5)
+search.grid(row=0,column=0)
+search.get()
 
-exit_button = Button(x, text='Exit', pady=10, padx=20, command=exit_task)
-exit_button.pack()
+spacing_label1 = Label(x,text=' ').grid(row=2)
+spacing_label2 = Label(x,text=' ').grid(row=3)
+spacing_label3 = Label(x,text=' ').grid(row=3)
+spacing_label4 = Label(x,text=' ').grid(row=4)
+
+
+def RESULTS():
+    data = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?q={search.get()}&appid=561873282dd9f39ea5aab7bd9f29e045')
+    weather_data = json.loads(data.text)
+    weather_result = Label(x,text=weather_data['weather'][0]['description'])
+    weather_result.grid(row=5,column=0)
+
+search_button = Button(x,text='search',command=lambda:RESULTS())
+search_button.grid(row=0,column=1)
 
 x.mainloop()
